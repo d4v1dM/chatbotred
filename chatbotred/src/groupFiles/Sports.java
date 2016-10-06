e492d4f4fd0a773c2f02356d84ab7c0267180e31
Student06 <Student06@10.8.33.104>
2016-10-06 12:19:57 -0400

    sports update3

--- chatbotred/src/groupFiles/Sports.java
+++ chatbotred/src/groupFiles/Sports.java
@@ -1,22 +1,25 @@
+//Kristy Tan
+
 package groupFiles;
 
 public class Sports implements Topic {
 	private boolean inSportsLoop;
 	private boolean inBaseballLoop;
 	private boolean inFootballLoop;
-	private String sportsResponse;   
+	private String sportsResponse;  
+	private boolean inTeamLoop;
 	
-	// create more responses  
-	private static String[] sport = {"I love sports.", "What sport do you like more? Football or Baseball?", "I'm not really a fan."}; // add more responses
+	
 	private static String[] football = {"Tom brady should be kicked out of the NFL", "the jets are not going to make playoffs this season.", "Broncos are doing well this season. Don't you think?", "I hope the NY Giants will win the superbowl again.", "Did you know that Lady Gaga is performing at this year's superbowl halftime?"};
 	private static String[] baseball = {"Mets are number 1 in the wild card race.", "Hopefully the Mets won't screw up the world series again", "Chase Utley should be banned from the MLB for breaking Tejada's leg"};
-	
+	private static String[] bteam = {"New York Mets", "San Fransico Giants", "Cleveland Cavaliers", "Miami Heat"};
+	private static String[] fteam = {"New York Jets", "New York Giants", "Denver Broncos", "New England Patriots", "Miami Dolphins"};
 	public void talk() {
 		inSportsLoop = true;
-		while(inSportsLoop){
+		while(inSportsLoop || inBaseballLoop || inFootballLoop){
 			printResponse();
 			sportsResponse = David.getInput();
-		
+			
 			if(!isTriggered(sportsResponse)){
 				inSportsLoop = false;
 				inBaseballLoop = false;
@@ -27,34 +30,47 @@ public class Sports implements Topic {
 	}
 
 	private void printResponse() {
-		int sIndex = (int) (Math.random() * sport.length);
 		int bIndex = (int) (Math.random() * baseball.length);
 		int fIndex = (int) (Math.random() * football.length);
-		if(inBaseballLoop && inSportsLoop){
-			David.print(baseball[bIndex]);
-		}
-		else
-		if(inFootballLoop && inSportsLoop){
-			David.print(football[fIndex]);
+		int btIndex = (int) (Math.random() * bteam.length);
+		int ftIndex = (int) (Math.random() * fteam.length);
+		
+		if(inBaseballLoop){
+			if(inTeamLoop){
+				David.print("My favorite team is " + bteam[btIndex] + ".");
+			}
+			else{
+				David.print(baseball[bIndex]);
+			}
 		}
 		else{
-			David.print(sport[sIndex]);
+			if(inFootballLoop){
+				if(inTeamLoop){
+					David.print("My favorite team is " + fteam[ftIndex] + ".");
+				}
+				else{
+					David.print(football[fIndex]);
+				}
+			}
 		}
 	}
 
 	public boolean isTriggered(String userInput) {
 		if(David.findKeyword(userInput, "sports", 0) >= 0){
-			return true;
+			return inSportsLoop = true;
 		}
-		if(David.findKeyword(userInput, "baseball", 0) >= 0){
-			inBaseballLoop = true;
-			inSportsLoop = true;
+		if(inSportsLoop && David.findKeyword(userInput, "baseball", 0) >= 0){
+			inFootballLoop = false;
+			return inBaseballLoop = true;
 		}
-		if(David.findKeyword(userInput, "football", 0) >= 0){
-			inFootballLoop = true;
-			inSportsLoop = true;
+		if(inSportsLoop && David.findKeyword(userInput, "football", 0) >= 0){
+			inBaseballLoop = false; 
+			return inFootballLoop = true;
+			
+		}
+		if(inSportsLoop && David.findKeyword(userInput, "team", 0) >= 0){
+			return inTeamLoop = true;
 		}
 		return false;
 	}
-
 }
