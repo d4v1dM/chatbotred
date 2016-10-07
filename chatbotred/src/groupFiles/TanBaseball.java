4444994ae84dbbb3915865f834a867dd6e6f1168
Kristy <Kristy@kristys-mbp.home>
2016-10-06 20:48:36 -0400

    need to fix

--- chatbotred/src/groupFiles/TanBaseball.java
+++ chatbotred/src/groupFiles/TanBaseball.java
@@ -5,8 +5,12 @@ package groupFiles;
 public class TanBaseball implements Topic {
 
 	private boolean inBaseballLoop1;
+	private boolean nameRight;
 	private String bResponse;
-	private static String[] bQuestions = {"Who is on first base", "Who just hit a grand slam?", "Who is pitching today?", "Who is batting right now?", "Who threw the ball to 3rd base?"};
+	private int spaces;
+	private static String[] bQuestions = {"Who is on first base", "Who just hit a grand slam?", "Who is pitching today?", "Who is batting right now?", 
+										 "Who threw the ball to 3rd base?", "Who just got banned from MLB?", "Who got a home run?", "Who struck out?", 
+							   			 "Who is the ace pitcher?", "Who lead their team to victory in the World Series?"};
 	
 	public void talk() {
 		inBaseballLoop1 = true;
@@ -14,49 +18,83 @@ public class TanBaseball implements Topic {
 			printResponse();
 			bResponse = David.getInput();
 			isName(bResponse);
-//			if(isName(bResponse)){
-//				printResponse();
-//				bResponse = David.getInput(); 
-//			}
-//			
-//			else if(!isTriggered(bResponse)){
-//				inBaseballLoop1 = false;
-//				David.talkForever();
-//			}
+
 		}
 	}
 
 	private void printResponse() { 
-		int responseIndex = (int)(Math.random() * bQuestions.length);
-		David.print(bQuestions[responseIndex]);
-		
-	}
-	
-	public void isName(String userInput){
 		String[] randomAnswer = {"Correct.", "Wrong."};
 		int ranIndex = (int) (Math.random() * randomAnswer.length);
-		int spaces = 0; 
-	
-		for(int s = 0; s < userInput.length(); s++){
-			if(userInput.charAt(s) == ' '){
-				spaces++;
+		int responseIndex = (int)(Math.random() * bQuestions.length);
+		
+		David.print(bQuestions[responseIndex]);
+		
+		if(nameRight == false){
+			if(spaces > 2){
+				David.print("I do not understand, Please just give me a name.");
+				bResponse = David.getInput();
+				isName(bResponse);
 			}
 		}
-		
-		if(spaces > 2){
-			David.print("I do not understand, Please just give me a name.");
-			bResponse = David.getInput();
-			isName(bResponse);
-		}
 		else{
-			if(randomAnswer[ranIndex] == "Correct."){
+			if(randomAnswer[ranIndex] == "Correct." || randomAnswer[ranIndex] == "Yes."){
 				David.print(randomAnswer[ranIndex] + bResponse + " did");
+				David.print(bQuestions[responseIndex]);
 			}
 			else{
 				David.print(randomAnswer[ranIndex] + bResponse + " did not.");
+				David.print(bQuestions[responseIndex]);
 			}	
 		}
 	}
+		
+	
+	public boolean isName(String userInput){
+		
+		
+		for(int s = 0; s < userInput.length(); s++){
+			if(userInput.charAt(s) == ' '){
+				spaces++;	
+			}
+		}
+		
+		if(spaces >= 2){
+			nameRight = false;
+			System.out.println("false");
+		}
+		else{
+			nameRight = true;
+			System.out.println("true");
+		}
+		return nameRight;
+	}
+	
+//	public void isName(String userInput){
+//		String[] randomAnswer = {"Correct.", "Wrong."};
+//		int ranIndex = (int) (Math.random() * randomAnswer.length);
+//		int spaces = 0;
+//	
+//		for(int s = 0; s < userInput.length(); s++){
+//			if(userInput.charAt(s) == ' '){
+//				spaces++;
+//			}
+//		}
+//		
+//		if(spaces > 2){
+//			David.print("I do not understand, Please just give me a name.");
+//			bResponse = David.getInput();
+//			isName(bResponse);
+//		}
+//		else{
+//			if(randomAnswer[ranIndex] == "Correct." || randomAnswer[ranIndex] == "Yes."){
+//				David.print(randomAnswer[ranIndex] + bResponse + " did");
+//			}
+//			else{
+//				David.print(randomAnswer[ranIndex] + bResponse + " did not.");
+//					}	
+//		}
+//		
+//	}
 	
 	public boolean isTriggered(String userInput) {
 		if(David.findKeyword(userInput, "sports", 0) >= 0){
